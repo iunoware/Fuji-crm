84b279e053fd4645858ed251fccb13bd550e0b0c
Praveen123-png <praveenkumar814444@gmail.com>
2026-06-01 12:57:13 +0530

    file structure done, materials page added, sidebar modified

---
+++ src/app/(dashboard)/materials/page.tsx
@@ -0,0 +1,7 @@
+export default function Materials() {
+  return (
+    <section className="h-screen w-screen text-md text-gray-700 grid place-items-center">
+      <h1>This is Materials page</h1>
+    </section>
+  );
+}
